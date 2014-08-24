3c2714ac291556c5f0e960faefb952405cd5520c
Diptamay Sanyal <diptamay@gmail.com>
2014-08-24 02:25:36 -0400

    Non working cut

--- src/test/scala/online/auctionbids/AuctionActorSpec.scala
+++ src/test/scala/online/auctionbids/AuctionActorSpec.scala
@@ -2,7 +2,7 @@ package online.auctionbids
 
 import org.scalatest.FunSpec
 
-class HelloWorldSpec extends FunSpec {
+class AuctionActorSpec extends FunSpec {
   describe("Adding 1 to 1") {
     it("should equals 2"){
       assert(1+1 == 2)
