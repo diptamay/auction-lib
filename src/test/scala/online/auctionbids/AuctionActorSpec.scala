b2db821a904e6bc9a947c31f36356f8ad7452dc8
Diptamay Sanyal <diptamay@gmail.com>
2014-08-24 15:43:56 -0400

    Adding unit tests

--- src/test/scala/online/auctionbids/AuctionActorSpec.scala
+++ src/test/scala/online/auctionbids/AuctionActorSpec.scala
@@ -15,6 +15,8 @@ class AuctionActorSpec extends TestKit(ActorSystem("testSystem")) with WordSpecL
   val item1           = Item("test-item-1", 10.0)
   val item2           = Item("test-item-2", 20.0)
   val auctioneer1     = Auctioneer("test-auctioneer-1")
+  val bidder1         = Bidder("test-bidder-1")
+  val bidder2         = Bidder("test-bidder-2")
   val auctionActorRef = TestActorRef(new AuctionActor)
 
   override def afterAll() {
@@ -47,23 +49,14 @@ class AuctionActorSpec extends TestKit(ActorSystem("testSystem")) with WordSpecL
   }
 
   "Auctioneer should" should {
-    "inquire valid auction status and get back status" in {
-      auctionActorRef ? Call(item1, auctioneer1) map {
+    "inquire valid auction and get back status" in {
+      auctionActorRef ? Inquire(item1) map {
         case Status(auction) =>
           println(s"Got auction status $auction")
           assert(auction.status == Running)
           assert(auction.highestBid == None)
           assert(auction.highestBidder == None)
           assert(auction.closedAt == None)
-        case _ => assert(false)
-      }
-    }
-  }
-
-  "Auctioneer should" should {
-    "inquire invalid auction status and get back not found" in {
-      auctionActorRef ? Call(item2, auctioneer1) map {
-        case NotFound => assert(true)
         case _ => assert(false)
       }
     }
@@ -78,6 +71,41 @@ class AuctionActorSpec extends TestKit(ActorSystem("testSystem")) with WordSpecL
           assert(auction.highestBid == None)
           assert(auction.highestBidder == None)
           assert(auction.closedAt != None)
+        case _ => assert(false)
+      }
+    }
+  }
+
+  "Auctioneer should" should {
+    "inquire invalid auction and get back not found" in {
+      auctionActorRef ? Inquire(item2) map {
+        case NotFound => assert(true)
+        case _ => assert(false)
+      }
+    }
+  }
+
+  "Auctioneer should" should {
+    "not be able to add already auction item" in {
+      auctionActorRef ? Add(item1, auctioneer1) map {
+        case Status(auction) =>
+          println(s"Got auction status $auction")
+          assert(auction.status != NotStarted)
+          assert(auction.item == item1)
+          assert(auction.auctioneer == auctioneer1)
+        case _ => assert(false)
+      }
+    }
+  }
+
+  "Auctioneer should" should {
+    "not be able to start an already auction item" in {
+      auctionActorRef ? Start(item1, auctioneer1) map {
+        case Status(auction) =>
+          println(s"Got auction status $auction")
+          assert(auction.status != Running)
+          assert(auction.item == item1)
+          assert(auction.auctioneer == auctioneer1)
         case _ => assert(false)
       }
     }
